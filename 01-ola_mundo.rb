94cb661550d6ebd60be9995642c18e1e7edaf872
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-08-19 17:05:48 -0300

    coloquei anotações sobre tipos primitivos

---
+++ 01-ola_mundo.rb
@@ -0,0 +1,6 @@
+# Exibe a mensagem 'Ola, Mundo!' no console
+puts 'Ola, Mundo!'
+
+# Calcula a soma de 1 + 2 e exibe o resultado no console
+# O uso de #{} dentro das aspas duplas permite interpolação de código Ruby
+puts "#{1 + 2}"
---
+++ 02-entrada_e_saida.rb
@@ -0,0 +1,4 @@
+puts "Olá!"
+puts "Digite o seu nome: "
+nome = gets 
+puts "Seu nome é: " + nome
