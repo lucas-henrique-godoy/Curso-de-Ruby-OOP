a7f8e681477891f05dd0b8b22678966336e6147c
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-08-20 11:27:52 -0300

    criei o coercao.rb

--- 02-entrada_e_saida.rb
+++ 02-entrada_e_saida.rb
@@ -1,4 +1,7 @@
 puts "Olá!"
 puts "Digite o seu nome: "
-nome = gets 
-puts "Seu nome é: " + nome
+nome = gets # Essa linha serve para ler a aentrada dpo teclado.
+puts "O seu nome é: " + nome
+puts "Digite sua idade: "
+idade = gets.chomp
+puts "Sua idade é: " + idade
---
+++ 03-strings.rb
@@ -0,0 +1,12 @@
+# INTERPOLAÇÃO DE VARIÁVEIS:
+
+# EX01
+v1 = 'Lucas'
+puts "Hello! " + v1
+
+#EX02
+v2 = "Lucas Henrique"
+puts "Beleza? #{v2}"
+
+#EX03
+puts "A soma de 1 + 2 é: #{1 + 2}"
