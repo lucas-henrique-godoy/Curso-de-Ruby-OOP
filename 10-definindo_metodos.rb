be982340991d12f2515c5738b1dc63e3f467677a
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-08-27 23:05:07 -0300

    Update 10-definindo_metodos.rb

--- 10-definindo_metodos.rb
+++ 10-definindo_metodos.rb
@@ -1,10 +1,15 @@
 class Pessoa
   def gritar
-    puts "Grrrhhhhhh"
+    puts "Grrrhhhhhh!!!"
   end
 
   def agradecer
+    puts "Obrigado!"
   end
 end
+##########################################33
+obj1 = Pessoa.new # Instanciando um objeto da classe pessoa
+obj1. gritar # Utilizando o método gritar da classe Pessoa
+obj1.agradecer # Utilizando o método agradecer da classe Pessoa
 
 
