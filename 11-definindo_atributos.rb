315f9a90c695a40b53de0013e1228146e41f2645
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-08-30 22:54:48 -0300

    Update 11-definindo_atributos.rb

--- 11-definindo_atributos.rb
+++ 11-definindo_atributos.rb
@@ -1,47 +1,44 @@
 class Pessoa
-  def initialize
-    @nome = nil
-    @idade = nil
-  end
+  @nome = nil
+  @idade = nil
 
-  def set_nome(nome)
+  def nome=(nome)
     @nome = nome
   end
 
-  def get_nome
+  def nome
     @nome
   end
 
-  def set_idade(idade)
+  def idade=(idade)
     @idade = idade
   end
 
-  def get_idade
+  def idade
     @idade
   end
 end
 
-#   def gritar(texto = "Grrrhhhhh!")
-#     "Gritando... #{texto}"
-#   end
+  def gritar(texto = "Grrrhhhhh!")
+    "Gritando... #{texto}"
+  end
 
-#   def agradecer(texto = "Obrigado!")
-#     texto
-#   end
-# end
-
-#################################################################################
+  def agradecer(texto = "Obrigado!")
+    texto
+  end
+ end
 
 pessoa1 = Pessoa.new
-pessoa1.set_nome("Lucas")
-pessoa1.set_idade(27)
+pessoa1.nome = "Lucas"
+pessoa1.idade = 27
 
 pessoa2 = Pessoa.new
-pessoa2.set_nome("Wendel")
-pessoa2.set_idade(49)
+pessoa2.nome = "Wendel"
+pessoa2.idade = 49
 
-puts pessoa1.get_nome
-puts pessoa1.get_idade
+puts pessoa1.nome
+puts pessoa1.idade
 
-puts pessoa2.get_nome
-puts pessoa2.get_idade
+puts pessoa2.nome
+puts pessoa2.idade
+
