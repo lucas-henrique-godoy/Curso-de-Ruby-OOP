fcf996e65d2f3ca2dea1f600bcc32fa423ef9a5e
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-03 22:26:30 -0300

    Update 15-overriding.rb

--- 15-overriding.rb
+++ 15-overriding.rb
@@ -1,4 +1,17 @@
 class Calculadora
+  def somar(n1, n2)
+    n1 + n2
+  end 
+end
 
+class CalculadoraFashion < Calculadora
 
 end
+
+###############################
+
+c = Calculadora.new
+puts c.somar(2,3)
+
+cf = CalculadoraFashion.new
+puts cf.somar(2,3)
