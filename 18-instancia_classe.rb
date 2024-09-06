85d8d199018b13dcc90ddebfcc4a02661893a457
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-06 14:23:39 -0300

    Update 18-instancia_classe.rb

--- 18-instancia_classe.rb
+++ 18-instancia_classe.rb
@@ -1,10 +1,21 @@
 class Teste
-    def ola
-      "Olá!"
-            
+    def ola # Método de Instância. É necessário instânciar para usar esse método.
+      "Olá!"            
+    end
+
+    def self.hello # Método de Classe. Não é preciso instânciar para usar esse método.
+      "Hello!"            
     end
 end
 
+# Usando o método de instância.
 obj = Teste.new
 puts obj.ola
+
+#___________________________________________________________________________________________
+
+# Usando o método de classe.
+puts Teste.hello
+
+
 
