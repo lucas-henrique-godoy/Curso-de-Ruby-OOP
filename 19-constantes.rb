586ebc85470b9f3a7821b0843e25388271d7bfc8
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-07 22:29:28 -0300

    Create 19-constantes.rb

---
+++ 19-constantes.rb
@@ -0,0 +1,20 @@
+class Teste
+  PI = 3.14
+end
+
+puts Teste::PI
+
+# Estou definindo uma constante PI dentro da classe Teste e depois imprimindo o valor dessa constante.
+
+# Explicação:
+# Definição da Classe e Constante:
+# class Teste define uma nova classe chamada Teste.
+# PI = 3.14 define uma constante PI com o valor 3.14 dentro da classe Teste.
+# Acesso à Constante:
+# Teste::PI acessa a constante PI da classe Teste.
+# Impressão:
+# puts Teste::PI imprime o valor da constante PI no console.
+# Resultado:
+# Quando você executa o código, a saída será:
+# 3.14
+# Isso acontece porque Teste::PI acessa a constante PI definida dentro da classe Teste e puts imprime seu valor.
