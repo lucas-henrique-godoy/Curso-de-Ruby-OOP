f5a2ff8b929c51a4a6925e34aa32c5d333744459
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-12 15:56:15 -0300

    adicionei arquivos gemfile e alguns exemplos de instalação de gems com  o bundle

---
+++ usando_o_bundler/app.rb
@@ -0,0 +1,7 @@
+# Requer a gem 'lerolero_generator' que foi instalada através do Bundler.
+# Esta gem é utilizada para gerar texto aleatório e fictício.
+require 'lerolero_generator'
+
+# Usa o método 'paragraph' da classe 'LeroleroGenerator' para gerar um parágrafo fictício.
+# O método 'paragraph' cria um bloco de texto aleatório que pode ser utilizado para testes ou exemplos.
+puts LeroleroGenerator.paragraph
