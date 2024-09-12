5936cc7d7fa8e4130b4a90693cff77d56639b958
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-12 15:05:31 -0300

    Create 21-gems.rb

---
+++ 21-gems.rb
@@ -0,0 +1,7 @@
+# gem install lerolero_generator
+require 'lerolero_generator'
+
+puts LeroleroGenerator.paragraph
+
+# SAÍDA: PS C:\Users\lucasgodoy\Desktop\Curso-de-Ruby-OOP> ruby "c:\Users\lucasgodoy\Desktop\Curso-de-Ruby-OOP\21-gems.rb"
+# Por outro lado, a complexidade dos estudos efetuados oferece uma boa oportunidade de verificação das nossas opções de desenvolvimento futuro. Por outro lado, o desenvolvimento de formas distintas de atuação facilita a definição das nossas opções de desenvolvimento futuro. A prática mostra que a análise dos diversos resultados prejudica a percepção da importância das opções básicas para o sucesso do programa. Caros colegas a complexidade dos estudos efetuados acarreta um processo de reformulação das nossas opções de desenvolvimento futuro. Caros colegas a atual estrutura de organização prejudica a percepção da importância das formas de ação.
