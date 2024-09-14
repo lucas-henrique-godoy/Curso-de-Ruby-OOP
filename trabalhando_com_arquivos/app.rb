6c0a06e624a365f865bdb794d64fdd4f0b2957b2
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-13 22:16:01 -0300

    Update app.rb

--- trabalhando_com_arquivos/app.rb
+++ trabalhando_com_arquivos/app.rb
@@ -1,9 +1,16 @@
-puts ARGV
+# puts ARGV[0]
+# puts ARGV.size
 
-puts "--------------------------------------"
 
-File.open('teste.txt', 'r') do |arq|
-  while line = arq.gets
-    puts line
+if ARGV.size > 0
+  File.open(ARGV[0], 'r') do |arq|
+    while line = arq.gets
+      puts line
+    end
   end
+else
+  puts "Você deve informar o nome do arquivo a ser aberto. Ex: app.rb teste.txt"
 end
+
+
+
