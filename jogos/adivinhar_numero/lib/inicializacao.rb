e6d559d3948068408adff8de30474d274a7d7920
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-16 16:19:40 -0300

    modifiquei o sistema do jogo de adivinhar números para incluir comandos de so por meio do ruby.

--- jogos/adivinhar_numero/lib/inicializacao.rb
+++ jogos/adivinhar_numero/lib/inicializacao.rb
@@ -1,10 +1,14 @@
 class Inicializacao
   def self.inicializando
+    system('cls')
+
     print "Inicializando."
     4.times do |i|
       sleep 1
       print "."
     end
-    puts "."      
+    puts "."
+    
+    system('cls')
   end
 end
