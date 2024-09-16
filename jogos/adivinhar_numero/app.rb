c494ab1415d0fe0f94db78b270bd6f949ac379ce
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-16 11:48:41 -0300

    exclui os arquivos do jogo para refazer

--- jogos/adivinhar_numero/app.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'lib\adivinhar_numero'
-
-jogo = AdivinharNumero.new
-
-until jogo.venceu do
-  puts "Digite um número: "
-  numero = gets.to_i
-  puts jogo.tentar_adivinhar(numero)
-end
