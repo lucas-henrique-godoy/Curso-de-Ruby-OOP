44c086d818120c200bb558a7b422c190e58801d1
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-17 12:59:53 -0300

    estou usando ascii art no projeto de adivinhar jogo

--- jogos/adivinhar_numero/lib/inicializacao.rb
+++ jogos/adivinhar_numero/lib/inicializacao.rb
@@ -2,13 +2,19 @@ class Inicializacao
   def self.inicializando
     system('cls')
 
-    print "Inicializando."
-    4.times do |i|
-      sleep 1
-      print "."
-    end
-    puts "."
-        
+    # print "Inicializando."
+    # 4.times do |i|
+    #   sleep 1
+    #   print "."
+    # end
+    # puts "."
+
+                         
+    puts " __                     " 
+    puts "|  |   _ _ ___ ___ ___  " 
+    puts "|  |__| | |  _| .'|_ -| " 
+    puts "|_____|___|___|__,|___| " 
+    sleep 3
     system('cls')
   end
 end
