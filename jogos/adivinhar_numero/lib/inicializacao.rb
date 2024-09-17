448809c056f6e36136face6cda1a0ebc85ad9e73
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-17 15:23:32 -0300

    Update inicializacao.rb

--- jogos/adivinhar_numero/lib/inicializacao.rb
+++ jogos/adivinhar_numero/lib/inicializacao.rb
@@ -2,20 +2,21 @@ class Inicializacao
   def self.inicializando
     system('cls')
 
-    # print "Inicializando."
-    # 4.times do |i|
-    #   sleep 1
-    #   print "."
-    # end
-    # puts "."
-                         
-    File.open(File.expand_path('../../ascii/bat.txt', __FILE__), 'r') do |arq|
-      while line = arq.gets
-        puts line
-      end
+    print "Inicializando."
+    4.times do |i|
+      sleep 1
+      print "."
     end
+    puts "."
+     
+    # usando um arquivo com ASCII Art
+    # File.open(File.expand_path('../../ascii/bat.txt', __FILE__), 'r') do |arq|
+    #   while line = arq.gets
+    #     puts line
+    #   end
+    # end
     
-    sleep 3
+    #sleep 3
 
     system('cls')
   end
