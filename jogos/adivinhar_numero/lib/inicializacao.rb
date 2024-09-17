65bd0d68b95c7cb7c34d07a44705fae60b885b5f
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-17 16:36:38 -0300

    carreguei mas não utilizei a gem de tabela em ascii art.

--- jogos/adivinhar_numero/lib/inicializacao.rb
+++ jogos/adivinhar_numero/lib/inicializacao.rb
@@ -1,4 +1,6 @@
 require 'tty-cursor'
+require 'tty-spinner'
+require 'tty-table'
 
 class Inicializacao
   def self.inicializando
@@ -7,12 +9,18 @@ class Inicializacao
     cursor = TTY::Cursor
     print cursor.move_to(60, 15)
 
-    print "Inicializando."
-    4.times do |i|
-      sleep 1
-      print "."
-    end
-    puts "."
+    spinner = TTY::Spinner.new
+    spinner = TTY::Spinner.new("[:spinner] Carregando ...", format: :pulse_2)
+    spinner.auto_spin # Automatic animation with default interval
+    sleep(5) # Perform task
+    spinner.stop("Pronto!") # Stop animation    
+
+    # print "Inicializando."
+    # 4.times do |i|
+    #   sleep 1
+    #   print "."
+    # end
+    # puts "."
      
     # usando um arquivo com ASCII Art
     # File.open(File.expand_path('../../ascii/bat.txt', __FILE__), 'r') do |arq|
