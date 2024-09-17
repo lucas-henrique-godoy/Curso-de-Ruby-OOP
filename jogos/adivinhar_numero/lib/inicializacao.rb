8719699d71482b6e7af47e356a7d55a8a5665c09
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-17 16:00:17 -0300

    usando gems de terminal

--- jogos/adivinhar_numero/lib/inicializacao.rb
+++ jogos/adivinhar_numero/lib/inicializacao.rb
@@ -1,6 +1,11 @@
+require 'tty-cursor'
+
 class Inicializacao
   def self.inicializando
     system('cls')
+
+    cursor = TTY::Cursor
+    print cursor.move_to(60, 15)
 
     print "Inicializando."
     4.times do |i|
